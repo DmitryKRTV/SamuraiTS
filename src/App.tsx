ffc7da8dd0449458d4273c7981c4b0e48a4151f1
DmitryKRTV <dmitrykorotaev.job@gmail.com>
2022-06-14 20:31:30 +0300

    Commit 1. Finished first 10 lessons (including) of react_samurai_v1 using TypeScript.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header/Header";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header/>
+      <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+      </ul>
     </div>
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <div><a href={"#"}>Home</a></div>
+            <div><a href={"#"}>New Feed</a></div>
+            <div><a href={"#"}>Messages</a></div>
+        </div>
+    );
+};
+
+export default Header;
